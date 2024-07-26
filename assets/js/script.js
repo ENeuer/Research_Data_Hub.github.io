b8ea184ed6bc1adb8af934d5386b033a5e92824f
ENeuer <eneuer@outlook.de>
2024-07-26 10:10:51 +0200

    Update style, js, landingpage and projectpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 // script.js
-
 document.addEventListener('DOMContentLoaded', () => {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('.nav-menu');
+    const burger = document.getElementById('burger');
+    const navLinks = document.getElementById('navLinks');
 
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.toggle('nav-menu-visible');
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-menu-visible');
     });
 
     // Smooth scrolling for the "Portfolio" button
     const portfolioButton = document.querySelector('a[href="#projects"]');
-    portfolioButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        document.querySelector('#projects').scrollIntoView({
-            behavior: 'smooth'
+    if (portfolioButton) {
+        portfolioButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector('#projects').scrollIntoView({
+                behavior: 'smooth'
+            });
         });
-    });
+    }
 });
-
 
